c3ab05e1cbaeb386301f90e61d8d9aa079c6b56d
joevandebilt <joevandebilt@live.co.uk>
2014-12-13 23:56:28 +0000

    Initial Commit v1.0.0
    Created Repo

---
+++ Gift-Spinner/frmGiftSpinner.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace GiftPicker
+{
+    partial class frmGiftSpinner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSpin = new System.Windows.Forms.Button();
+            this.cmdQuit = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.rbtFindOne = new System.Windows.Forms.RadioButton();
+            this.rbtFindMany = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // cmdSpin
+            // 
+            this.cmdSpin.Location = new System.Drawing.Point(45, 86);
+            this.cmdSpin.Name = "cmdSpin";
+            this.cmdSpin.Size = new System.Drawing.Size(80, 39);
+            this.cmdSpin.TabIndex = 0;
+            this.cmdSpin.Text = "Find a Gift";
+            this.cmdSpin.UseVisualStyleBackColor = true;
+            this.cmdSpin.Click += new System.EventHandler(this.cmdSpin_Click);
+            // 
+            // cmdQuit
+            // 
+            this.cmdQuit.Location = new System.Drawing.Point(131, 86);
+            this.cmdQuit.Name = "cmdQuit";
+            this.cmdQuit.Size = new System.Drawing.Size(80, 39);
+            this.cmdQuit.TabIndex = 1;
+            this.cmdQuit.Text = "Quit";
+            this.cmdQuit.UseVisualStyleBackColor = true;
+            this.cmdQuit.Click += new System.EventHandler(this.cmdQuit_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(50, 22);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(138, 16);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Number of Attempts: 0";
+            this.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(240, 12);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(571, 113);
+            this.rtbOutput.TabIndex = 3;
+            this.rtbOutput.Text = "";
+            // 
+            // rbtFindOne
+            // 
+            this.rbtFindOne.AutoSize = true;
+            this.rbtFindOne.Location = new System.Drawing.Point(45, 51);
+            this.rbtFindOne.Name = "rbtFindOne";
+            this.rbtFindOne.Size = new System.Drawing.Size(68, 17);
+            this.rbtFindOne.TabIndex = 4;
+            this.rbtFindOne.TabStop = true;
+            this.rbtFindOne.Text = "Find One";
+            this.rbtFindOne.UseVisualStyleBackColor = true;
+            // 
+            // rbtFindMany
+            // 
+            this.rbtFindMany.AutoSize = true;
+            this.rbtFindMany.Location = new System.Drawing.Point(131, 51);
+            this.rbtFindMany.Name = "rbtFindMany";
+            this.rbtFindMany.Size = new System.Drawing.Size(74, 17);
+            this.rbtFindMany.TabIndex = 5;
+            this.rbtFindMany.TabStop = true;
+            this.rbtFindMany.Text = "Find Many";
+            this.rbtFindMany.UseVisualStyleBackColor = true;
+            // 
+            // frmGiftSpinner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 146);
+            this.Controls.Add(this.rbtFindMany);
+            this.Controls.Add(this.rbtFindOne);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.cmdQuit);
+            this.Controls.Add(this.cmdSpin);
+            this.Name = "frmGiftSpinner";
+            this.Text = "Gift Spinner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSpin;
+        private System.Windows.Forms.Button cmdQuit;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.RadioButton rbtFindOne;
+        private System.Windows.Forms.RadioButton rbtFindMany;
+    }
+}
+
---
+++ Gift-Spinner/frmGiftSpinner.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace GiftPicker
+{
+    public partial class frmGiftSpinner : Form
+    {
+        bool CloseThread;
+        object myLock = new object();
+        Thread PageFinder;
+        int StartStop = 0;
+        bool FindOne;
+
+        public frmGiftSpinner()
+        {
+            InitializeComponent();
+            rbtFindOne.Select();
+            FindOne = true;
+        }
+
+        private void cmdQuit_Click(object sender, EventArgs e)
+        {
+            lblOutput.Text = "Quitting";
+            lock (myLock)
+            {
+                CloseThread = true;
+            }
+            try
+            {
+                PageFinder.Join();
+            }
+            catch { }
+            Application.Exit();
+        }
+
+        private void cmdSpin_Click(object sender, EventArgs e)
+        {
+            if (StartStop == 0) //Thread is not running
+            {
+                CloseThread = false;
+                rtbOutput.Clear();
+
+
+                if (rbtFindMany.Checked == true)
+                {
+                    cmdSpin.Text = "Stop";
+                    FindOne = false;
+                    StartStop = 1;
+                }
+                else if (rbtFindOne.Checked == true)
+                {
+                    cmdSpin.Text = "Find a Gift";
+                    FindOne = true;
+                    StartStop = 0;
+                }                
+                PageFinder = new Thread(FindRandomPage);
+                PageFinder.Start();
+            }
+            else
+            {
+                lock (myLock)
+                {
+                    CloseThread = true;
+                }
+                PageFinder.Join();
+                cmdSpin.Text = "Start";
+                StartStop = 0;
+            }
+            //FindRandomPage();
+        }
+
+        private void FindRandomPage()
+        {
+            HttpWebRequest request;
+            HttpWebResponse response;
+            bool PageFound = false;
+            string URL = "http://www.amazon.co.uk/dp/product/B00";
+            int Tries = 0;
+            while (!CloseThread && !PageFound)
+            {
+                string PageURL = URL + GenerateRandomString(7) + '/';
+                //string PageURL = "http://www.amazon.co.uk/dp/product/B00CIV6990/";    //Test Success
+                //string PageURL = "http://www.amazon.co.uk/dp/product/B00CFART0/";     //Test Failure
+                try
+                {
+                    Output(PageURL);
+                    request = (HttpWebRequest)WebRequest.Create(PageURL);
+                    request.Timeout = 1000;
+                    response = (HttpWebResponse)request.GetResponse();
+                    response.Dispose();
+
+                    if (FindOne == true) PageFound = true;
+                    System.Diagnostics.Process.Start(PageURL);
+                    Output(" - Success");
+                }
+                catch (Exception ex) //404 more than likely
+                {
+                    PageFound = false;
+                    if (ex.Message.Contains("404"))
+                    {
+                        Output(" - 404");
+                    }
+                    else Output(" - "+ ex.Message);
+                }
+                Tries++;
+                Output(Environment.NewLine);
+
+                if (this.lblOutput.InvokeRequired)
+                {
+                    this.lblOutput.BeginInvoke((MethodInvoker)delegate() { this.lblOutput.Text = "Number of Attempts: " + Tries; ;});
+                }
+                else
+                {
+                    this.lblOutput.Text = "Number of Attempts: " + Tries;
+                }                
+            }
+        }
+
+        private static Random random = new Random();
+        private string GenerateRandomString (int Length)
+        {
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            int NextChar ;
+            for (int i = 0; i < Length; i++)
+            {
+                NextChar = random.Next(48, 90);
+                while (NextChar > 57 && NextChar < 65)
+                {
+                    NextChar = random.Next(48, 90);
+                }
+                ch = Convert.ToChar(NextChar);
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+
+        private void Output(string Input)
+        {
+
+            if (this.rtbOutput.InvokeRequired)
+            {
+                this.rtbOutput.BeginInvoke((MethodInvoker)delegate() { this.rtbOutput.AppendText(Input); ;});
+            }
+            else
+            {
+                this.rtbOutput.AppendText(Input);
+            }
+        }
+    }
+}
